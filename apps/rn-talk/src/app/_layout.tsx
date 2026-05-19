6fc894a435c96fab3c1f7b88bf6c2fb3028a1a52
Shane Zheng <shane.zheng@linkecu.com>
2026-05-19 22:13:43 +1200

    enable React StrictMode
    so that npx react-compiler-healthcheck@latest passes

--- apps/rn-talk/src/app/_layout.tsx
+++ apps/rn-talk/src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StrictMode } from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
@@ -7,9 +8,11 @@ import { AppTabs } from '@/components/app-tabs';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <StrictMode>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+      </ThemeProvider>
+    </StrictMode>
   );
 }
